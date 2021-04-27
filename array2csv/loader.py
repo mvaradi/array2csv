3bc46b8bed52104a335530c6f1e5d991fbd1257d
mvaradi <mvaradi@ebi.ac.uk>
2021-04-27 16:04:04 +0100

    Adding methods for aggregating matrix data

--- array2csv/loader.py
+++ array2csv/loader.py
@@ -1,7 +1,8 @@
 import sys
 import ntpath
 
-from numpy import load, amin, amax
+import numpy as np
+# from numpy import load, amin, amax, ceil
 
 
 class Loader(object):
@@ -20,7 +21,7 @@ class Loader(object):
 
         :return: None
         """
-        archive = load(self.path)
+        archive = np.load(self.path)
         self.data = archive[archive.files[0]][0]
 
     def get_data(self):
@@ -47,7 +48,70 @@ class Loader(object):
 
         :return: Tuple, min and max values in the data
         """
-        return amin(self.data), amax(self.data)
+        return np.amin(self.data), np.amax(self.data)
+
+    def get_number_of_iterations(self, window):
+        """
+        This method calculates the number of iterations required by the create_aggregated_data() method
+        :param window: Number, window size
+        :return: Number
+        """
+        dimension = len(self.data)
+        number_of_iterations = np.square(np.ceil(dimension / window))
+        return number_of_iterations
+
+    def create_aggregated_data(self, window_size):
+        """
+        This method takes an input n x n numpy array-of-arrays (i.e. matrix) and creates a new matrix
+        that has the average or an i x i size slice matrix in a new matrix
+
+        For example:
+        input = [[0,1],[2,3]]
+        output with a window of 2x2 = [[1.5]]
+        :return: Numpy array of arrays
+        """
+
+        """
+        Get the number of iterations needed to slide a window through the matrix
+        The window first moves through a row (from left to right) and then goes
+        down and starts again at the beginning of the row
+        """
+        iterations = self.get_number_of_iterations(window_size)
+        x = 0
+        y = 0
+        data = []
+        for i in range(int(iterations)):
+            values = []
+            values.append(self.data[x][y])
+            if y+1 < len(self.data[x]):
+                values.append(self.data[x][y+1])
+            if x+1 < len(self.data):
+                values.append(self.data[x+1][y])
+            if x + 1 < len(self.data) and y+1 < len(self.data[x+1]):
+                values.append(self.data[x+1][y+1])
+            data.append(np.mean(values))
+
+            if y + 2 > len(self.data[x]) - 1:
+                if y + 1 > len(self.data[x]) - 1:
+                    # The window reached the end of the row, so rewind it to 0
+                    y = 0
+                    if x + 2 > len(self.data) - 1:
+                        if x + 1 > len(self.data) - 1:
+                            # The window reached the end of the bottom row, so end the loop
+                            break
+                        else:
+                            x += 1
+                    else:
+                        x += 2
+                else:
+                    y += 1
+            else:
+                y += 2
+        return self.make_aggregated_matrix(data, iterations)
+
+    def make_aggregated_matrix(self, data, iterations):
+        dimension = int(np.sqrt(iterations))
+        return np.array(data).reshape(dimension, dimension)
 
 
 # if __name__ == "__main__":
